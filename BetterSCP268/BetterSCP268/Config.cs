be336c06c11980142bd4e7b74930f313c18f1750
lifyxus <lifyxus@gmail.com>
2022-05-05 20:31:31 +0300

    Refactoring

--- BetterSCP268/BetterSCP268/Config.cs
+++ BetterSCP268/BetterSCP268/Config.cs
@@ -1,35 +1,35 @@
-﻿
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using Exiled.API.Interfaces;
+
 namespace BetterSCP268
 {
     public class Config : IConfig
     {
         [Description("Whether or not this plugin is enabled")]
-        public bool IsEnabled { get; set; } = true;
+        public bool IsEnabled { get; set; } = false;
 
-        [Description("No damage will be dealt to the player with SCP-268")]  
+        [Description("No damage will be dealt to the player with SCP-268")]
         public bool CanDamage { get; set; } = true;
 
         [Description("it will show broadcast to the player if there is a player next to SCP-268")]
         public string SendBroadcast { get; set; } = "<b>A player with <color=#4fd411>SCP-268</color> next to you </b>";
 
-        [Description("Tesla will be disabled if SCP-268 goes through the tesla")]  
-        public bool DiableTesla { get; set; } = true;
+        [Description("Tesla will be disabled if SCP-268 goes through the tesla")]
+        public bool DisableTesla { get; set; } = true;
 
         [Description("SCP-096 can't see if player uses SCP-268")]
-        public bool SCP096Trigger { get; set; } = true;
+        public bool Scp096Trigger { get; set; } = true;
 
         [Description("The distance between players from which the broadcast will show")]
         public int Scp330Distance { get; set; } = 16;
 
         [Description("Time the loop waits before loopig again, increase for more performance, but less accurate position broadcasts")]
-        public ushort broadcastDelay { get; set; } = 1;  
+        public ushort BroadcastDelay { get; set; } = 1;
 
         [Description("You won't get damage when you will fall with SCP-268(false = you won't get damage, true = you will get)")]
-        public bool CanFalldown { get; set; } = false;
+        public bool CanFallDown { get; set; } = false;
 
         [Description("You won't get damage when you will be near SCP-244 (false = you won't get damage, true = you will get)")]
-        public bool DisableSCP244Damage { get; set; } = false;
+        public bool DisableScp244Damage { get; set; } = false;
     }
 }
--- BetterSCP268/BetterSCP268/EventHandlers.cs
+++ BetterSCP268/BetterSCP268/EventHandlers.cs
@@ -1,86 +1,72 @@
-﻿
-using Exiled.API.Features;
+﻿using Exiled.API.Features;
 using Exiled.Events.EventArgs;
 using CustomPlayerEffects;
-using Exiled.API.Features.Items;
 using System.Collections.Generic;
 using MEC;
 using UnityEngine;
-using Exiled.API.Enums;
 
 namespace BetterSCP268
 {
     public class EventHandlers
     {
-        public Dictionary<Player, CoroutineHandle> playerhandlelist = new Dictionary<Player, CoroutineHandle>();
-        public Better268 plugin;
-        public EventHandlers(Better268 plugin) => this.plugin = plugin;
+        private readonly Dictionary<Player, CoroutineHandle> _playerHandleList = new Dictionary<Player, CoroutineHandle>();
         
-        public void OnHurt268(HurtingEventArgs ev)
+        private readonly Config _config = Plugin.Instance.Config;
+        
+        public void OnHurting(HurtingEventArgs ev)
         {
-            if (plugin.Config.CanDamage && ev.Target.GetEffectActive<Invisible>())
-                ev.IsAllowed = false; 
-        }  
+            if (_config.CanDamage && ev.Target.GetEffectActive<Invisible>())
+                ev.IsAllowed = false;
+        }
 
         public void OnTriggeringTesla(TriggeringTeslaEventArgs ev)
         {
-            if (plugin.Config.DiableTesla && ev.Player.GetEffectActive<Invisible>())
+            if (_config.DisableTesla && ev.Player.GetEffectActive<Invisible>())
                 ev.IsTriggerable = false;
-        } 
+        }
 
         public void OnUsingSCP(UsingItemEventArgs ev)
         {
-            if (!playerhandlelist.ContainsKey(ev.Player) && ev.Item.Type == ItemType.SCP268)  
-               playerhandlelist.Add(ev.Player, Timing.RunCoroutine(DetectCoroutine(ev.Player)));
-            
-        } 
+            if (!_playerHandleList.ContainsKey(ev.Player) && ev.Item.Type == ItemType.SCP268) 
+                _playerHandleList.Add(ev.Player, Timing.RunCoroutine(DetectCoroutine(ev.Player)));
+        }
 
         public void OnAddingTarget(AddingTargetEventArgs ev)
         {
-            if (plugin.Config.SCP096Trigger && ev.Target.GetEffectActive<Invisible>())
+            if (_config.Scp096Trigger && ev.Target.GetEffectActive<Invisible>())
                 ev.IsAllowed = false;
         }
-        public void OnDamage(HurtingEventArgs ev)
-        {
-            if (ev.Target.GetEffectActive<Invisible>())  
-            {
-                switch (ev.Handler.Type)
-                {
-                    case DamageType.Falldown:
-                        ev.IsAllowed = plugin.Config.CanFalldown;
-                        return;
-                    case DamageType.Hypothermia:
-                        ev.IsAllowed = plugin.Config.DisableSCP244Damage;
-                        return;
 
-                }
-            }
-        }
-        public IEnumerator<float> DetectCoroutine(Player Scp330Player)
+        public void OnRoundEnd(EndingRoundEventArgs ev)
         {
-            for (; ; )
+            foreach (var item in _playerHandleList)
+            {
+                Timing.KillCoroutines(item.Value);
+            }
+
+            _playerHandleList.Clear();
+        }
+        
+        private IEnumerator<float> DetectCoroutine(Player scp330Player)
+        {
+            for (;;)
             {
                 foreach (Player player in Player.List)
                 {
-                    if (player == Scp330Player) continue;
-                    if (Vector3.Distance(player.Position, Scp330Player.Position) <= plugin.Config.Scp330Distance) 
-                        player.Broadcast(plugin.Config.broadcastDelay, plugin.Config.SendBroadcast);
+                    if (player == scp330Player) continue;
+                    
+                    if (Vector3.Distance(player.Position, scp330Player.Position) <= _config.Scp330Distance)
+                        player.Broadcast(_config.BroadcastDelay, _config.SendBroadcast);
                 }
-                if (!Scp330Player.GetEffectActive<Invisible>())
+
+                if (!scp330Player.GetEffectActive<Invisible>())
                 {
-                    Timing.KillCoroutines(playerhandlelist[Scp330Player]);
-                    playerhandlelist.Remove(Scp330Player);
+                    Timing.KillCoroutines(_playerHandleList[scp330Player]);
+                    _playerHandleList.Remove(scp330Player);
                 }
-                yield return Timing.WaitForSeconds(plugin.Config.broadcastDelay);
+
+                yield return Timing.WaitForSeconds(_config.BroadcastDelay);
             }
-        } 
-        public void OnRoundEnd(EndingRoundEventArgs ev)
-        {
-            foreach (var item in playerhandlelist)
-            {
-                Timing.KillCoroutines(item.Value);
-            } 
-            playerhandlelist.Clear();
-        } 
+        }
     }
 }
